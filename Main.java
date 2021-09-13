c82db74d2544de31f4b3d7784acd0d35b74e556e
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-09-13 21:04:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    // 1. psuedo code if statement
+
+    // Accepting full time job offer
+
+/*   
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Starting Salary:");
+    int salary = s.nextInt();
+    int goal = 75000;
+
+    if (salary > 50000 && salary < goal){
+      System.out.println("Negotiate more money.");
+    }
+    else if (salary > goal){
+      System.out.println("Accept the offer.");
+    }
+    else{
+      System.out.println("Reject the offer.");
+    }
+*/
+  
+
+    // 2. psuedo code loop
+
+    // things to juggle this semester (weekly)
+
+    int work = 24; //total work hours
+    int inClass = 15; //total credit hours
+    int busy = 39; //total hours busy
+
+    for {
+      System.out.println (work);
+      System.out.println (inClass);
+      while (busy == inClass + work);
+      if (busy = 39){
+        break;
+      }
+
+     if (busy == 39){
+       System.out.println ("All things complete");
+     }
+    
+    
+
+  }
+}
