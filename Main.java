e5dbe6ce8dc872ac0de17406c814894cb4abf55b
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-09-13 21:39:46 +0000

    while loop wont work

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ class Main {
 
     // Accepting full time job offer
 
-/*   
+   
     Scanner s = new Scanner(System.in);
 
     System.out.println("Starting Salary:");
@@ -23,27 +23,29 @@ class Main {
     else{
       System.out.println("Reject the offer.");
     }
-*/
+
   
 
     // 2. psuedo code loop
 
     // things to juggle this semester (weekly)
+    // couldnt get this to work in java
 
 /*
-    int work = 24; //total work hours
-    int inClass = 15; //total credit hours
-    int busy = 39; //total hours busy
+    int work = 24; //total work hours (weekly)
+    int inClass = 15; //total credit hours (weekly)
+    int service = 1; //total service hours (weekly)
+    int busy = 40; //total hours busy (weekly)
 
     for {
       System.out.println (work);
       System.out.println (inClass);
       while (busy == inClass + work);
-      if (busy = 39){
+      if (busy = 40){
         break;
       }
 
-     if (busy == 39){
+     if (busy == 40){
        System.out.println ("All things complete");
      }
  */   
